2af819dd1dd7ab959769fc5dd7d6df41aa967c34
Lucas de Oliveira Cunha <lsch@gft.com>
2020-03-05 18:08:04 -0300

    Projeto de APIREST com Angular 4

---
+++ algamoneyapi/src/main/java/com/exemple/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.exemple.algamoneyapi.repository;
+
+import com.exemple.algamoneyapi.model.Categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
